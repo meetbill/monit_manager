66e6795d7d80bbd861e501087a8ec3a00bc2f696
BillWang139967 <meetbill@163.com>
2017-05-16 11:11:45 +0800

    update:doc

--- README.md
+++ README.md
@@ -5,3 +5,32 @@ monit 管理工具
 
 原理性内容会往 wiki 上整理
 [wiki](https://github.com/BillWang139967/monit_manager/wiki)
+
+## version
+
+* v0.1，2017-05-16 init: 可以获取 monit 状态信息并转换为 json
+
+## 参加步骤
+
+* 在 GitHub 上 `fork` 到自己的仓库，然后 `clone` 到本地，并设置用户信息。
+```
+$ git clone https://github.com/BillWang139967/monit_manager.git
+$ cd monit_manager
+$ git config user.name "yourname"
+$ git config user.email "your email"
+```
+* 修改代码后提交，并推送到自己的仓库。
+```
+$ #do some change on the content
+$ git commit -am "Fix issue #1: change helo to hello"
+$ git push
+```
+* 在 GitHub 网站上提交 pull request。
+* 定期使用项目仓库内容更新自己仓库内容。
+```
+$ git remote add upstream https://github.com/BillWang139967/monit_manager.git
+$ git fetch upstream
+$ git checkout master
+$ git rebase upstream/master
+$ git push -f origin master
+```
--- monit_lib/main.py
+++ monit_lib/main.py
@@ -6,17 +6,27 @@
 
 # File Name: main.py
 # Description:
+# version:0.1
 
 """
 import requests
 import my_lib.xmldict
+import sys
 
 class Monit(dict):
     def __init__(self, host='localhost', port=2812, username=None, password='', https=False):
         self.baseurl = (https and 'https://%s:%i' or 'http://%s:%i') % (host, port)
         if username:
             self.auth = requests.auth.HTTPBasicAuth(username, password)
-    
+        try:
+            r = requests.get(self.baseurl,auth=self.auth)
+            if not (r.status_code == 200):
+                print("the password is not right")
+                sys.exit()
+        except Exception,e:
+            #print Exception,":",e
+            print("the monit is not running")
+            sys.exit()
     def update(self):
         """
         Update Monit deamon and services status.
